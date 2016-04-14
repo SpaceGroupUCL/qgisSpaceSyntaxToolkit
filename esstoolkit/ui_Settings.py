db78d2ee8ac16bdb117b440fe167f79474e2f1ab
Jorge Gil <gil.jorge@gmail.com>
2016-04-14 08:19:24 +0200

    resource update

--- esstoolkit/ui_Settings.py
+++ esstoolkit/ui_Settings.py
@@ -2,8 +2,7 @@
 
 # Form implementation generated from reading ui file '/Users/jorge/github/qgisSpaceSyntaxToolkit/esstoolkit/ui_Settings.ui'
 #
-# Created: Fri Jul  3 20:14:00 2015
-#      by: PyQt4 UI code generator 4.10.3
+# Created by: PyQt4 UI code generator 4.11.4
 #
 # WARNING! All changes made in this file will be lost!
 
